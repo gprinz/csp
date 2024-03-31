443bbde03d37559e6f91de9b004e8b8cf29c51d1
prinzg <prinzg@student.ethz.ch>
2024-03-31 19:36:13 +0200

    Adapt pipeline

--- main.tf
+++ main.tf
@@ -1,18 +1,18 @@
- terraform {
-       backend "remote" {
-         # The name of your Terraform Cloud organization.
-         organization = "CSP-ETHZ"
+terraform {
+  backend "remote" {
+    # The name of your Terraform Cloud organization.
+    organization = "CSP-ETHZ"
 
-         # The name of the Terraform Cloud workspace to store Terraform state files in.
-         workspaces {
-           name = "main"
-         }
-       }
-     }
+    # The name of the Terraform Cloud workspace to store Terraform state files in.
+    workspaces {
+      name = "main"
+    }
+  }
+}
 
-     # An example resource that does nothing.
-     resource "null_resource" "example" {
-       triggers = {
-         value = "A example resource that does nothing!"
-       }
-     }
+# An example resource that does nothing.
+resource "null_resource" "example" {
+  triggers = {
+    value = "A example resource that does nothing!"
+  }
+}
