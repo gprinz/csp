9196d8634186b65580d488b1437d303f342dab23
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-14 18:42:46 +0200

    Add source

--- terraform/data_factory.tf
+++ terraform/data_factory.tf
@@ -8,7 +8,7 @@ resource "azurerm_storage_account" "datalake" {
 }
 
 # Create Data Lake Gen2 Filesystem
-resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
+resource "azurerm_storage_data_lake_gen2_filesystem" "dl_fs" {
   name               = "dlfs-${local.current_year}-ch"
   storage_account_id = azurerm_storage_account.datalake.id
 }
@@ -23,4 +23,22 @@ resource "azurerm_data_factory_linked_service_azure_blob_storage" "source" {
   name              = "Open Data"
   data_factory_id   = azurerm_data_factory.df.id
   connection_string = "DefaultEndpointsProtocol=https;AccountName=azureopendatastorage;EndpointSuffix=blob.core.windows.net"
+}
+
+resource "azurerm_data_factory_dataset_parquet" "source_parquet" {
+  name                = "source-parquet"
+  data_factory_id     = azurerm_data_factory.df.id
+  linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.source.name
+
+  azure_blob_storage_location {
+    container = "nyctlc"
+    path      = "yellow/"
+  }
+}
+
+resource "azurerm_data_factory_linked_service_data_lake_storage_gen2" "destination" {
+  name                 = "destination-parquet"
+  data_factory_id      = azurerm_data_factory.df.id
+  url                  = azurerm_storage_data_lake_gen2_filesystem.dl_fs.primary_blob_endpoint
+  use_managed_identity = true
 }
