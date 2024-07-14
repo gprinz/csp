9b91c3a06490e87e44a71884f3dcdd95765e75db
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-14 16:47:36 +0200

    Major imporvements

--- terraform/data_factory.tf
+++ terraform/data_factory.tf
@@ -1,117 +1,122 @@
-# Resource Group
-resource "azurerm_resource_group" "rg" {
-  name     = "data-factory-resources"
+provider "azurerm" {
+  features {}
+}
+
+# Create Resource Group
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
   location = "West Europe"
 }
 
-# Data Factory
-resource "azurerm_data_factory" "data_factory" {
-  name                = "data-factory"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-}
+# Create Data Lake Storage Account
+resource "azurerm_storage_account" "datalake" {
+  name                     = "examplestorageacc"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
 
-# Data Factory Linked Service for Blob Storage
-resource "azurerm_data_factory_linked_service_azure_blob_storage" "blob_storage" {
-  name            = "blobstorage-linkedservice"
-  data_factory_id = azurerm_data_factory.data_factory.id
-
-  connection_string = "DefaultEndpointsProtocol=wbs;AccountName=youraccountname;AccountKey=youraccountkey;EndpointSuffix=core.windows.net"
-}
-
-# Data Factory Dataset for Transactions Source Data
-resource "azurerm_data_factory_dataset_parquet" "transactions_source" {
-  name                = "transactions-source-dataset"
-  data_factory_id     = azurerm_data_factory.data_factory.id
-  linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.blob_storage.name
-
-  azure_blob_storage_location {
-    container = "feature-store-prp-1"
-    path      = "datasources / transactions-source"
-    filename  = "test.parquet"
+  blob_properties {
+    container_delete_retention_policy {
+      days = 7
+    }
   }
-
 }
 
-#resource "azurerm_data_factory_pipeline" "pipeline" {
-#  name            = "data-pipeline"
-#  data_factory_id = azurerm_data_factory.data_factory.id
-#
-#  activities_json = <<JSON
-#  [
-#    {
-#      "name": "CopyFromBlobToADLS",
-#      "type": "Copy",
-#      "dependsOn": [],
-#      "typeProperties": {
-#        "source": {
-#          "type": "ParquetSource"
-#        },
-#        "sink": {
-#          "type": "AzureDataLakeStoreSink",
-#          "linkedServiceName": {
-#            "referenceName": "${azurerm_data_factory_linked_service_azure_data_lake_store.adls.name}",
-#            "type": "LinkedServiceReference"
-#          }
-#        }
-#      },
-#      "inputs": [
-#        {
-#          "referenceName": "${azurerm_data_factory_dataset_parquet.transactions_source.name}",
-#          "type": "DatasetReference"
-#        }
-#      ],
-#      "outputs": [
-#        {
-#          "referenceName": "${azurerm_data_factory_dataset_parquet.transactions_source.name}",
-#          "type": "DatasetReference"
-#        }
-#      ]
-#    }
-#  ]
-#  JSON
-#}
-## Data Factory Trigger for Daily Execution
-#resource "azurerm_data_factory_trigger_schedule" "daily_trigger" {
-#  name            = "daily-trigger"
-#  data_factory_id = azurerm_data_factory.data_factory.id
-#
-#  pipeline_name = azurerm_data_factory_pipeline.pipeline.name
-#
-#  frequency = "Day"
-#  interval  = 1
-#}
+# Create Data Lake Gen2 Filesystem
+resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
+  name               = "example-filesystem"
+  storage_account_id = azurerm_storage_account.datalake.id
+}
 
-## Storage Account
-#resource "azurerm_storage_account" "example" {
-#  name                     = "examplestorageacct"
-#  resource_group_name      = azurerm_resource_group.rg.name
-#  location                 = azurerm_resource_group.rg.location
-#  account_tier             = "Standard"
-#  account_replication_type = "LRS"
-#  is_hns_enabled           = true # This enables the Data Lake Storage Gen2 capabilities
-#}
-#
-## Storage Container
-#resource "azurerm_storage_container" "example" {
-#  name                  = "example-container"
-#  storage_account_name  = azurerm_storage_account.example.name
-#  container_access_type = "private"
-#}
-#
-## Data Factory Linked Service for ADLS Gen2
-#resource "azurerm_data_factory_linked_service_azure_data_lake_store" "adls" {
-#  name                = "adls-linkedservice"
-#  resource_group_name = azurerm_resource_group.rg.name
-#  data_factory_name   = azurerm_data_factory.data_factory.name
-#
-#  data_lake_store_id = azurerm_storage_account.example.id
-#}
-#
-## Permissions for Data Factory to access Data Lake Store
-#resource "azurerm_role_assignment" "role_assignment" {
-#  principal_id         = azurerm_data_factory.data_factory.identity.0.principal_id
-#  role_definition_name = "Contributor"
-#  scope                = azurerm_storage_account.example.id
-#}
-#
+
+
+resource "azurerm_data_factory" "example" {
+  name                = "exampledf"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+}
+
+resource "azurerm_data_factory_linked_service_azure_blob_storage" "source" {
+  name              = "example-linked-service-source"
+  data_factory_id   = azurerm_data_factory.example.id
+  connection_string = "DefaultEndpointsProtocol=https;AccountName=stmdwpublic;EndpointSuffix=blob.core.windows.net"
+}
+
+
+resource "azurerm_data_factory_dataset_azure_blob" "source_dataset" {
+  name                = "example-source-dataset"
+  data_factory_id     = azurerm_data_factory.example.id
+  linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.source.name
+  filename            = "*.parquet"
+}
+
+
+resource "azuread_application" "example" {
+  display_name = "example-app"
+}
+
+
+resource "azurerm_data_factory_linked_service_azure_blob_storage" "destination" {
+  name              = "example-linked-service-destination"
+  data_factory_id   = azurerm_data_factory.example.name
+  connection_string = azurerm_storage_account.datalake.primary_connection_string
+}
+
+resource "azurerm_data_factory_linked_service_azure_data_lake_storage_gen2" "source" {
+  name                 = "example-linked-service-source"
+  resource_group_name  = azurerm_resource_group.example.name
+  data_factory_name    = azurerm_data_factory.example.name
+  storage_account_name = azurerm_storage_account.datalake.name
+}
+
+# Datasets
+resource "azurerm_data_factory_dataset_azure_blob" "destination_dataset" {
+  name                = "example-destination-dataset"
+  data_factory_id     = azurerm_data_factory.example.id
+  linked_service_name = azurerm_data_factory_linked_service_azure_blob_storage.destination.name
+}
+
+resource "azurerm_data_factory_pipeline" "example_pipeline" {
+  name            = "example-pipeline"
+  data_factory_id = azurerm_data_factory.example.id
+
+  dynamic "activity" {
+    for_each = [
+      {
+        name    = "GetMetadata"
+        type    = "GetMetadata"
+        inputs  = [azurerm_data_factory_dataset_azure_data_lake_storage_gen2.source_dataset.id]
+        outputs = []
+        settings = jsonencode({
+          fieldList = ["Child Items"]
+        })
+      },
+      {
+        name = "ForEach"
+        type = "ForEach"
+        settings = jsonencode({
+          items = "@activity('GetMetadata').output.childItems"
+        })
+        activities = [
+          {
+            name    = "CopyData"
+            type    = "Copy"
+            inputs  = [azurerm_data_factory_dataset_azure_data_lake_storage_gen2.source_dataset.id]
+            outputs = [azurerm_data_factory_dataset_azure_blob.destination_dataset.id]
+            settings = jsonencode({
+              source = {
+                type = "ParquetSource"
+              },
+              sink = {
+                type = "BlobSink"
+              }
+            })
+          }
+        ]
+      }
+    ]
+  }
+}
+
+data "azurerm_client_config" "example" {}
