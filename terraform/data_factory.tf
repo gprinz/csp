bfa6efdc24965e77644c05da893f8ca99ee07a0a
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-14 18:24:35 +0200

    Add source

--- terraform/data_factory.tf
+++ terraform/data_factory.tf
@@ -13,14 +13,14 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "example" {
   storage_account_id = azurerm_storage_account.datalake.id
 }
 
-resource "azurerm_data_factory" "example" {
+resource "azurerm_data_factory" "df" {
   name                = "df-${local.current_year}-ch"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.rg_prod.name
+  location            = azurerm_resource_group.rg_prod.location
 }
 
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "source" {
   name              = "example-linked-service-source"
-  data_factory_id   = azurerm_data_factory.example.id
+  data_factory_id   = azurerm_data_factory.cv.id
   connection_string = "DefaultEndpointsProtocol=https;AccountName=stmdwpublic;EndpointSuffix=blob.core.windows.net"
 }
