0579b700bf7528c4a75f5c974768d9ee1f12abab
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-14 15:03:46 +0200

    Fix Terraform error 5

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,24 +2,20 @@
 variable "ARM_SUBSCRIPTION_ID" {
   description = "The subscription ID for Azure"
   type        = string
-  default     = "Placeholder"
 }
 
 variable "ARM_CLIENT_ID" {
   description = "The client ID for Azure"
   type        = string
-  default     = "Placeholder"
 }
 
 variable "ARM_CLIENT_SECRET" {
   description = "The client secret for Azure"
   type        = string
   sensitive   = true
-  default     = "Placeholder"
 }
 
 variable "ARM_TENANT_ID" {
   description = "The tenant ID for Azure"
   type        = string
-  default     = "Placeholder"
 }
