651703fe0b56ee5f127742ae8d3a7da505767f41
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-14 20:38:43 +0200

    Anonyoumous Authentciation

--- terraform/data_factory.tf
+++ terraform/data_factory.tf
@@ -13,22 +13,31 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "dl_fs" {
   storage_account_id = azurerm_storage_account.datalake.id
 }
 
+// Define a user-assigned managed identity
+resource "azurerm_user_assigned_identity" "uai_df" {
+  name                = "uai-datafactory"
+  resource_group_name = azurerm_resource_group.rg_prod.name
+  location            = azurerm_resource_group.rg_prod.location
+}
+
+// Define an Azure Data Factory instance
 resource "azurerm_data_factory" "df" {
   name                = "df-${local.current_year}-ch"
   resource_group_name = azurerm_resource_group.rg_prod.name
   location            = azurerm_resource_group.rg_prod.location
 
   identity {
-    type = "SystemAssigned"
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.uai_df.id]
   }
 }
 
+// Assign the "Storage Blob Data Contributor" role to the managed identity
 resource "azurerm_role_assignment" "raw_data_storage" {
-  principal_id         = azurerm_data_factory.df.identity[0].principal_id
+  principal_id         = azurerm_user_assigned_identity.uai_df.principal_id
   role_definition_name = "Storage Blob Data Contributor"
   scope                = azurerm_storage_account.raw_data.id
 }
-
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "source" {
   name              = "Open_Data"
   data_factory_id   = azurerm_data_factory.df.id
