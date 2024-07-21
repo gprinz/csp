28700bef243ec9722a40b1b8b7be73419a6045e4
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-21 20:48:15 +0200

    OK

--- terraform/synapse.tf
+++ terraform/synapse.tf
@@ -19,6 +19,14 @@ resource "azurerm_storage_data_lake_gen2_filesystem" "file" {
   storage_account_id = azurerm_storage_account.synapse.id
 }
 
+# Example for allowing all Azure services to connect (not recommended for production)
+resource "azurerm_synapse_firewall_rule" "allow_azure_services" {
+  name                 = "allow-azure-services"
+  synapse_workspace_id = azurerm_synapse_workspace.synapse.id
+  start_ip_address     = "0.0.0.0"
+  end_ip_address       = "0.0.0.0"
+}
+
 resource "azurerm_synapse_workspace" "synapse" {
   name                                 = "synapse6335"
   resource_group_name                  = azurerm_resource_group.rg_synapse.name
@@ -30,6 +38,13 @@ resource "azurerm_synapse_workspace" "synapse" {
   identity {
     type = "SystemAssigned"
   }
+}
+
+# Assign Synapse Administrator role to the managed identity
+resource "azurerm_role_assignment" "synapse_admin_role" {
+  principal_id         = azurerm_synapse_workspace.synapse.identity[0].principal_id
+  role_definition_name = "Synapse Administrator"
+  scope                = azurerm_synapse_workspace.synapse.id
 }
 
 resource "azurerm_synapse_sql_pool" "sql" {
