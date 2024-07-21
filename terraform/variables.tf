2da43f0e8220f88d6383905e06963648d509feae
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-21 19:42:23 +0200

    New

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -29,3 +29,14 @@ variable "SYNAPSE_ADMIN_PWD" {
   description = "The tenant ID for Azure"
   type        = string
 }
+
+variable "azure_devops_org_service_url" {
+  description = "Azure DevOps Organization URL"
+  default     = "cps-ethz-2024"
+  type        = string
+}
+
+variable "azure_devops_pat" {
+  description = "Azure DevOps Personal Access Token"
+  type        = string
+}
