49c556508e39c41372ce3451f493fdf7121b111a
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-21 17:53:07 +0200

    Initial commit 21.7.24

--- terraform/df_pipeline.tf
+++ terraform/df_pipeline.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group_template_deployment" "NYCTaxi" {
 
   template_content = <<TEMPLATE
   {
-    "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+    "$schema": "http://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
     "contentVersion": "1.0.0.0",
         "parameters": {
         "dataFactoryName": {
