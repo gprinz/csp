03813e853fdfa2c0278b33767e76a5a533c365fb
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-21 17:29:39 +0200

    Initial commit 21.7.24

--- terraform/df_pipeline.tf
+++ terraform/df_pipeline.tf
@@ -1,12 +1,25 @@
-resource "azurerm_template_deployment" "NYCTaxi" {
-  name                = "example-deployment"
+resource "azurerm_resource_group_template_deployment" "NYCTaxi" {
+  name                = "NYC Taxi"
   resource_group_name = azurerm_resource_group.rg_prod.name
   deployment_mode     = "Incremental"
+  parameters_content = jsonencode({
+    "dataFactoryName" = {
+      value = azurerm_data_factory.df.name
+    }
+  })
 
-  template_body = <<TEMPLATE
+  template_content = <<TEMPLATE
   {
     "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
     "contentVersion": "1.0.0.0",
+        "parameters": {
+        "dataFactoryName": {
+            "type": "string",
+            "metadata": {
+                "description": "Name of the Factory"
+            }
+        }
+    },
     "resources": [
       {
         "type": "Microsoft.DataFactory/factories/linkedservices",
@@ -15,7 +28,7 @@ resource "azurerm_template_deployment" "NYCTaxi" {
         "properties": {
           "type": "AzureBlobStorage",
           "typeProperties": {
-            "serviceEndpoint": "https://azureopendatastorage.blob.core.windows.net/nyctlc".
+            "serviceEndpoint": "https://azureopendatastorage.blob.core.windows.net/nyctlc",
             "authenticationType": "Anonymous"
           }
         }
@@ -28,16 +41,12 @@ resource "azurerm_template_deployment" "NYCTaxi" {
     }
   }
   TEMPLATE
-
-  parameters = {
-    dataFactoryName = azurerm_data_factory.df.name
-  }
 }
 
 resource "azurerm_data_factory_dataset_parquet" "yellow_taxi" {
   name                = "yellow_taxi"
   data_factory_id     = azurerm_data_factory.df.id
-  linked_service_name = azurerm_template_deployment.NYCTaxi.name
+  linked_service_name = azurerm_resource_group_template_deployment.NYCTaxi.name
 
   azure_blob_storage_location {
     path      = "yellow"
@@ -54,7 +63,7 @@ resource "azurerm_data_factory_dataset_parquet" "yellow_taxi" {
 resource "azurerm_data_factory_dataset_parquet" "green_taxi" {
   name                = "green_taxi"
   data_factory_id     = azurerm_data_factory.df.id
-  linked_service_name = azurerm_template_deployment.NYCTaxi.name
+  linked_service_name = azurerm_resource_group_template_deployment.NYCTaxi.name
 
   azure_blob_storage_location {
     path      = "green"
@@ -72,7 +81,7 @@ resource "azurerm_data_factory_dataset_parquet" "green_taxi" {
 resource "azurerm_data_factory_dataset_parquet" "taxi_fhv" {
   name                = "taxi_fhv"
   data_factory_id     = azurerm_data_factory.df.id
-  linked_service_name = azurerm_template_deployment.NYCTaxi.name
+  linked_service_name = azurerm_resource_group_template_deployment.NYCTaxi.name
 
   azure_blob_storage_location {
     path      = "fhv"
@@ -112,3 +121,74 @@ resource "azurerm_data_factory_dataset_parquet" "Parquet1" {
   }
 }
 
+
+resource "azurerm_data_factory_pipeline" "taxi_green_copy" {
+  name            = "pipeline1"
+  data_factory_id = azurerm_data_factory.df.id
+  variables = {
+    "bob" = "item1"
+  }
+  activities_json = <<JSON
+[
+    {
+        "name": "Copy Green Taxi Data",
+        "type": "Copy",
+        "dependsOn": [],
+        "policy": {
+            "timeout": "12:00:00",
+            "retry": 0,
+            "retryIntervalInSeconds": 30,
+            "secureOutput": false,
+            "secureInput": false
+        },
+        "userProperties": [],
+        "typeProperties": {
+            "source": {
+                "type": "ParquetSource",
+                "storeSettings": {
+                    "type": "AzureBlobStorageReadSettings",
+                    "recursive": true,
+                    "wildcardFileName": "*.parquet",
+                    "enablePartitionDiscovery": true
+                },
+                "formatSettings": {
+                    "type": "ParquetReadSettings"
+                }
+            },
+            "sink": {
+                "type": "ParquetSink",
+                "storeSettings": {
+                    "type": "AzureBlobStorageWriteSettings"
+                },
+                "formatSettings": {
+                    "type": "ParquetWriteSettings"
+                }
+            },
+            "enableStaging": false,
+            "translator": {
+                "type": "TabularTranslator",
+                "typeConversion": true,
+                "typeConversionSettings": {
+                    "allowDataTruncation": true,
+                    "treatBooleanAsNumber": false
+                }
+            }
+        },
+        "inputs": [
+            {
+                "referenceName": "green_taxi",
+                "type": "DatasetReference",
+                "parameters": {}
+            }
+        ],
+        "outputs": [
+            {
+                "referenceName": "Parquet1",
+                "type": "DatasetReference",
+                "parameters": {}
+            }
+        ]
+    }
+]
+  JSON
+}
