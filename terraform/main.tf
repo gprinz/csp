f1cbfa569ae48e47dd0ff46be16f15f02990f0cc
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-21 18:14:02 +0200

    Initial commit 21.7.24

--- terraform/main.tf
+++ terraform/main.tf
@@ -33,6 +33,14 @@ resource "azurerm_resource_group" "rg_prod" {
   location = "West Europe"
 }
 
+# Storage account configuration
+resource "azurerm_storage_account" "synapse" {
+  name                     = "sasynapse${local.current_year}ch"
+  location                 = azurerm_resource_group.rg_prod.location
+  resource_group_name      = azurerm_resource_group.rg_prod.name
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
 
 # Resource Group for machine learning
--- terraform/synapse.tf
+++ terraform/synapse.tf
@@ -1,16 +1,16 @@
 
 resource "azurerm_synapse_workspace" "synapse" {
   name                                 = "Synapse Analytics"
-  resource_group_name                  = azurerm_resource_group.example.name
-  location                             = azurerm_resource_group.example.location
-  storage_data_lake_gen2_filesystem_id = azurerm_storage_account.example.id
+  resource_group_name                  = azurerm_resource_group.rg_prod.name
+  location                             = azurerm_resource_group.rg_prod.location
+  storage_data_lake_gen2_filesystem_id = azurerm_storage_account.synapse.id
   sql_administrator_login              = var.SYNAPSE_ADMIN_USER
   sql_administrator_login_password     = var.SYNAPSE_ADMIN_PWD
 }
 
-resource "azurerm_synapse_sql_pool" "example" {
+resource "azurerm_synapse_sql_pool" "sql" {
   name                 = "DWH"
-  synapse_workspace_id = azurerm_synapse_workspace.example.id
+  synapse_workspace_id = azurerm_synapse_workspace.synapse.id
   sku_name             = "DW100c"
   create_mode          = "Default"
   storage_account_type = "GRS"
