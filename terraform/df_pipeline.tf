f3a1f617cfcfdb4403574a8dc9bef39244520436
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-21 17:30:16 +0200

    Initial commit 21.7.24

--- terraform/df_pipeline.tf
+++ terraform/df_pipeline.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group_template_deployment" "NYCTaxi" {
-  name                = "NYC Taxi"
+  name                = "NYCTaxi"
   resource_group_name = azurerm_resource_group.rg_prod.name
   deployment_mode     = "Incremental"
   parameters_content = jsonencode({
