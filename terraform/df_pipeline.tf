72050989b7ac9cd85e02d1535a7b8e67b9095751
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-21 16:21:14 +0200

    Initial commit 21.7.24

--- terraform/df_pipeline.tf
+++ terraform/df_pipeline.tf
@@ -1,79 +1,10 @@
 
-resource "azurerm_data_factory_pipeline" "taxi_green_copy" {
-  name            = "pipeline1"
-  data_factory_id = azurerm_data_factory.df.id
-  variables = {
-    "bob" = "item1"
-  }
-  activities_json = <<JSON
-[
-    {
-        "name": "Copy Green Taxi Data",
-        "type": "Copy",
-        "dependsOn": [],
-        "policy": {
-            "timeout": "12:00:00",
-            "retry": 0,
-            "retryIntervalInSeconds": 30,
-            "secureOutput": false,
-            "secureInput": false
-        },
-        "userProperties": [],
-        "typeProperties": {
-            "source": {
-                "type": "ParquetSource",
-                "storeSettings": {
-                    "type": "AzureBlobStorageReadSettings",
-                    "recursive": true,
-                    "wildcardFileName": "*.parquet",
-                    "enablePartitionDiscovery": true
-                },
-                "formatSettings": {
-                    "type": "ParquetReadSettings"
-                }
-            },
-            "sink": {
-                "type": "ParquetSink",
-                "storeSettings": {
-                    "type": "AzureBlobStorageWriteSettings"
-                },
-                "formatSettings": {
-                    "type": "ParquetWriteSettings"
-                }
-            },
-            "enableStaging": false,
-            "translator": {
-                "type": "TabularTranslator",
-                "typeConversion": true,
-                "typeConversionSettings": {
-                    "allowDataTruncation": true,
-                    "treatBooleanAsNumber": false
-                }
-            }
-        },
-        "inputs": [
-            {
-                "referenceName": "green_taxi",
-                "type": "DatasetReference",
-                "parameters": {}
-            }
-        ],
-        "outputs": [
-            {
-                "referenceName": "Parquet1",
-                "type": "DatasetReference",
-                "parameters": {}
-            }
-        ]
-    }
-]
-  JSON
-}
+
 
 resource "azurerm_data_factory_linked_service_azure_blob_storage" "NYCTaxi" {
-  name              = "NYCTaxi"
-  data_factory_id   = azurerm_data_factory.df.id
-  connection_string = "https://azureopendatastorage.blob.core.windows.net/nyctlc"
+  name             = "NYCTaxi"
+  data_factory_id  = azurerm_data_factory.df.id
+  service_endpoint = "https://azureopendatastorage.blob.core.windows.net/nyctlc"
 
 }
 
@@ -153,4 +84,75 @@ resource "azurerm_data_factory_dataset_parquet" "Parquet1" {
     name = "vendorID"
     type = "String"
   }
+}
+
+resource "azurerm_data_factory_pipeline" "taxi_green_copy" {
+  name            = "pipeline1"
+  data_factory_id = azurerm_data_factory.df.id
+  variables = {
+    "bob" = "item1"
+  }
+  activities_json = <<JSON
+[
+    {
+        "name": "Copy Green Taxi Data",
+        "type": "Copy",
+        "dependsOn": [],
+        "policy": {
+            "timeout": "12:00:00",
+            "retry": 0,
+            "retryIntervalInSeconds": 30,
+            "secureOutput": false,
+            "secureInput": false
+        },
+        "userProperties": [],
+        "typeProperties": {
+            "source": {
+                "type": "ParquetSource",
+                "storeSettings": {
+                    "type": "AzureBlobStorageReadSettings",
+                    "recursive": true,
+                    "wildcardFileName": "*.parquet",
+                    "enablePartitionDiscovery": true
+                },
+                "formatSettings": {
+                    "type": "ParquetReadSettings"
+                }
+            },
+            "sink": {
+                "type": "ParquetSink",
+                "storeSettings": {
+                    "type": "AzureBlobStorageWriteSettings"
+                },
+                "formatSettings": {
+                    "type": "ParquetWriteSettings"
+                }
+            },
+            "enableStaging": false,
+            "translator": {
+                "type": "TabularTranslator",
+                "typeConversion": true,
+                "typeConversionSettings": {
+                    "allowDataTruncation": true,
+                    "treatBooleanAsNumber": false
+                }
+            }
+        },
+        "inputs": [
+            {
+                "referenceName": "green_taxi",
+                "type": "DatasetReference",
+                "parameters": {}
+            }
+        ],
+        "outputs": [
+            {
+                "referenceName": "Parquet1",
+                "type": "DatasetReference",
+                "parameters": {}
+            }
+        ]
+    }
+]
+  JSON
 }
