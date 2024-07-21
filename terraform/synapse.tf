e4fa6726eb86e939cda6013bcd4bb9288425735b
Raphael Prinz <raphaelprinz@outlook.com>
2024-07-21 20:12:48 +0200

    OK

--- terraform/synapse.tf
+++ terraform/synapse.tf
@@ -1,12 +1,12 @@
 
 # Resource Group for production
 resource "azurerm_resource_group" "rg_synapse" {
-  name     = "rgsyn${local.current_year}ch3"
+  name     = "rgsyn${local.current_year}ch4"
   location = "West Europe"
 }
 
 resource "azurerm_synapse_workspace" "synapse" {
-  name                                 = "shng45"
+  name                                 = "shnghdffdfbd"
   resource_group_name                  = azurerm_resource_group.rg_synapse.name
   location                             = azurerm_resource_group.rg_synapse.location
   storage_data_lake_gen2_filesystem_id = azurerm_storage_account.synapse.id
