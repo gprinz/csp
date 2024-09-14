e038db714f58c8ed3f140a4f5d653b907f05a05e
Raphael Prinz <raphaelprinz@outlook.com>
2024-09-14 15:51:52 +0200

    Remove firewall rule

--- terraform/synapse.tf
+++ terraform/synapse.tf
@@ -8,8 +8,8 @@ resource "azurerm_resource_group" "rg_synapse" {
 # Storage account configuration
 resource "azurerm_storage_account" "synapse" {
   name                     = "sasynapse${local.current_year}ch"
-  location                 = azurerm_resource_group.rg_prod.location
-  resource_group_name      = azurerm_resource_group.rg_prod.name
+  location                 = azurerm_resource_group.rg_synapse.location
+  resource_group_name      = azurerm_resource_group.rg_synapse.name
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
@@ -17,6 +17,14 @@ resource "azurerm_storage_account" "synapse" {
 resource "azurerm_storage_data_lake_gen2_filesystem" "file" {
   name               = "example"
   storage_account_id = azurerm_storage_account.synapse.id
+}
+
+# Example for allowing all Azure services to connect (not recommended for production)
+resource "azurerm_synapse_firewall_rule" "allow_azure_services" {
+  name                 = "AllowAllWindowsAzureIps"
+  synapse_workspace_id = azurerm_synapse_workspace.synapse.id
+  start_ip_address     = "0.0.0.0"
+  end_ip_address       = "0.0.0.0"
 }
 
 resource "azurerm_synapse_workspace" "synapse" {
@@ -34,6 +42,13 @@ resource "azurerm_synapse_workspace" "synapse" {
 
 # Assign Synapse Administrator role to the managed identity
 resource "azurerm_role_assignment" "synapse_admin_role" {
+  principal_id         = azurerm_synapse_workspace.synapse.identity[0].principal_id
+  role_definition_name = "Synapse Administrator"
+  scope                = "/subscriptions/796313f9-881f-4bee-bd46-ba6ad10afbb4"
+}
+
+# Assign Synapse Administrator role to the managed identity
+resource "azurerm_role_assignment" "contributor_role" {
   principal_id         = azurerm_synapse_workspace.synapse.identity[0].principal_id
   role_definition_name = "Contributor"
   scope                = "/subscriptions/796313f9-881f-4bee-bd46-ba6ad10afbb4"
