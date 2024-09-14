af4ff23e10b5836b0fe8f56b397082ff6c456889
Raphael Prinz <raphaelprinz@outlook.com>
2024-09-14 17:55:15 +0200

    Adapt KV policy

--- terraform/data_factory.tf
+++ terraform/data_factory.tf
@@ -5,24 +5,6 @@ resource "azurerm_user_assigned_identity" "uai_df" {
   location            = azurerm_resource_group.rg_prod.location
 }
 
-// Define an Azure Data Factory instance
-resource "azurerm_data_factory" "df" {
-  name                = "df-${local.current_year}-ch"
-  resource_group_name = azurerm_resource_group.rg_prod.name
-  location            = azurerm_resource_group.rg_prod.location
-
-  identity {
-    type = "SystemAssigned"
-  }
-}
-
-# Assign Synapse Administrator role to the managed identity
-resource "azurerm_role_assignment" "df_admin_role" {
-  principal_id         = azurerm_data_factory.df.identity[0].principal_id
-  role_definition_name = "Contributor"
-  scope                = "/subscriptions/796313f9-881f-4bee-bd46-ba6ad10afbb4"
-}
-
 resource "azurerm_data_factory" "adf" {
   name                = "adf-${local.current_year}"
   resource_group_name = azurerm_resource_group.rg_synapse.name
