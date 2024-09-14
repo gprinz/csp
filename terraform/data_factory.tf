20c92afdfdfd99b3377f0d09d8d880adeeeb45f5
Raphael Prinz <raphaelprinz@outlook.com>
2024-09-14 18:02:10 +0200

    Improve code

--- terraform/data_factory.tf
+++ terraform/data_factory.tf
@@ -1,10 +1,3 @@
-// Define a user-assigned managed identity
-resource "azurerm_user_assigned_identity" "uai_df" {
-  name                = "uai-datafactory"
-  resource_group_name = azurerm_resource_group.rg_prod.name
-  location            = azurerm_resource_group.rg_prod.location
-}
-
 resource "azurerm_data_factory" "adf" {
   name                = "adf-${local.current_year}"
   resource_group_name = azurerm_resource_group.rg_synapse.name
@@ -14,7 +7,6 @@ resource "azurerm_data_factory" "adf" {
     type = "SystemAssigned"
   }
 }
-
 
 resource "azurerm_role_assignment" "adf_blob_contributor" {
   scope                = azurerm_storage_account.synapse.id
